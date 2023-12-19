620abef49976440e8eb567c51d5b3bf9209e1493
J M <wohaste@gmail.com>
2023-12-18 19:17:22 -0800

    user serialize/deserialize in progress

--- php/classes.php
+++ php/classes.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once("constants.php");
+include_once("global_utils.php");
 
 class Eft_User {
 	public $id;
@@ -12,6 +13,35 @@ class Eft_User {
 	public $phone_number;
 	public $last_login_date;
 	public $is_deactivated;
+
+	// returns string from serializing this user record
+	// end-line character is not included
+	public function serialize(?string $format) : string {
+		switch($format) {
+			case "1.0": return $this->serialize_1_0();
+			default: throw new Exception(MESSAGE_UNKNOWN_DATA_FORMAT);
+		}
+	}
+	
+	// returns string from serializing this data format version 1.0 user record
+	// end-line character is not included
+	private function serialize_1_0() : string {
+		return "{$this->id}|{$this->serialize_date($this->created_date)}|{$this->serialize_bool($this->is_admin)}|{$this->username}|{$this->password_hashed}|{$this->email}|{$this->phone_number}|{$this->serialize_date($this->last_login_date)}|{$this->serialize_bool($this->is_deactivated)}";
+	}
+	
+	private function serialize_date($date) : string {
+		try {
+			if($date == null) return ""; //shouldn't be necessary, but for some reason the try/catch is not working as expected
+			return $date->format('Ymd');
+		} 
+		catch (Exception $e) {
+			return "";
+		}		
+	}
+	
+	private function serialize_bool($bool) : string {
+		return ($bool) ? "1" : "0";
+	}
 
 	// returns object from deserializing a user record
 	public static function deserialize(?string $line, ?string $format) : ?Eft_User {
@@ -33,15 +63,15 @@ class Eft_User {
 			return null;
 		}
 		$user = new Eft_User();
-		$user->id = intval($matches[1]); //todo move the safe_get method to a shared location, use it here
-		$user->created_date = DateTime::createFromFormat('YYYYddmm', $matches[2]); //todo is there a just-Date class?
-		$user->is_admin = ($matches[3] == '1');
-		$user->username = $matches[4];
-		$user->password_hashed = $matches[5];
-		$user->email = $matches[6];
-		$user->phone_number = $matches[7];
-		$user->last_login_date = DateTime::createFromFormat('YYYYddmm', $matches[8]); //todo is there a just-Date class?
-		$user->is_deactivated = ($matches[9] == '1');
+		$user->id = intval(eft_get_element($matches, 1));
+		$user->created_date = DateTime::createFromFormat('Ymd', eft_get_element($matches, 2));
+		$user->is_admin = (eft_get_element($matches, 3) == '1');
+		$user->username = eft_get_element($matches, 4);
+		$user->password_hashed = eft_get_element($matches, 5);
+		$user->email = eft_get_element($matches, 6);
+		$user->phone_number = eft_get_element($matches, 7);
+		$user->last_login_date = DateTime::createFromFormat('Ymd', eft_get_element($matches, 8));
+		$user->is_deactivated = (eft_get_element($matches, 9) == '1');
 		
 		return $user;
 	}
--- php/tests/test_classes.php
+++ php/tests/test_classes.php
@@ -8,14 +8,74 @@ class TestClasses extends TestCase
 {
 	///////////////////////////////////
 	
+	public function testUser_Serialize_1_0_DefaultUser() : void
+	{
+		//Arrange
+		$user = new Eft_User();
+		$format = "1.0";
+		//Act
+		$result = $user->serialize($format);
+		//Assert
+		self::assertSame('||0||||||0', $result);
+	}
+	
+	public function testUser_Serialize_1_0_FullUser() : void
+	{
+		//Arrange
+		$user = new Eft_User();
+		$user->id = 1;
+		$user->created_date = DateTime::createFromFormat('Ymd', '20230131');
+		$user->is_admin = False;
+		$user->username = "jack";
+		$user->password_hashed = "abcde";
+		$user->email = "j@gmail.com";
+		$user->phone_number = "1234567890";
+		$user->last_login_date = DateTime::createFromFormat('Ymd', '20230315');
+		$user->is_deactivated = False;
+		$format = "1.0";
+		//Act
+		$result = $user->serialize($format);
+		//Assert
+		self::assertSame("1|20230131|0|jack|abcde|j@gmail.com|1234567890|20230315|0", $result);
+	}
+	
+	public function testUser_Serialize_1_0_Dates() : void
+	{
+		//Arrange
+		$user = $this->build_user();
+		$user->created_date = DateTime::createFromFormat('Ymd', '20230131'); //single digit month
+		$user->last_login_date = DateTime::createFromFormat('Ymd', '20231105'); //single digit day
+		$format = "1.0";
+		//Act
+		$result = $user->serialize($format);
+		//Assert
+		self::assertTrue(strpos($result, "20230131") !== False);
+		self::assertTrue(strpos($result, "20231105") !== False);
+	}
+	
+	public function testUser_Serialize_1_0_Bools() : void
+	{
+		//Arrange
+		$user = $this->build_user();
+		$user->is_admin = True;
+		$user->is_deactivated = False;
+		$format = "1.0";
+		//Act
+		$result = $user->serialize($format);
+		//Assert
+		self::assertTrue(strpos($result, "|1|".$user->username) !== False);
+		self::assertTrue(strpos($result, "|0") == strlen($result) - 2);
+	}
+	
 	public function testUser_Deserialize_NullLine_ThrowsException() : void
 	{
 		//Arrange
 		$line = null;
 		$format = "1.0";
-		$this->expectExceptionMessage(MESSAGE_UNKNOWN_DATA_FORMAT);
-		//Act Assert
+		//Act
 		$result = Eft_User::deserialize($line, $format);
+		//Assert
+		self::assertNull($result);
 	}
 	
 	public function testUser_Deserialize_UnknownVersion_ThrowsException() : void
@@ -28,24 +88,88 @@ class TestClasses extends TestCase
 		$result = Eft_User::deserialize($line, $format);
 	}
 	
-	public function testUser_Deserialize_1_0_AllFieldsFound() : void
+	public function testUser_Deserialize_1_0_InvalidFormat_ReturnsNull() : void
 	{
 		//Arrange
-		//Id|CreatedDate|IsAdmin|Username|PasswordHashed|Email|PhoneNumber|LastLoginDate|IsDeactivated
-		$line = "1|20230131|0|jack|abcde|j@gmail.com|1234567890|20230315|0";
+		$line = "abc|def";
 		$format = "1.0";
 		//Act
 		$result = Eft_User::deserialize($line, $format);
 		//Assert
-		
-		//todo
+		self::assertNull($result);
 	}
 	
-	//todo deserialize success
+	public function testUser_Deserialize_1_0_AllFieldsFound() : void
+	{
+		//Arrange
+		$user = $this->build_user();
+		$format = "1.0";
+		$line = $user->serialize($format);
+		//Act
+		$result = Eft_User::deserialize($line, $format);
+		//Assert
+		self::assertNotNull($result);
+		self::assertSame($user->id, $result->id);
+		self::assertSame($user->created_date->format('Y-m-d'), $result->created_date->format('Y-m-d'));
+		self::assertSame($user->is_admin, $result->is_admin);
+		self::assertSame($user->username, $result->username);
+		self::assertSame($user->password_hashed, $result->password_hashed);
+		self::assertSame($user->email, $result->email);
+		self::assertSame($user->phone_number, $result->phone_number);
+		self::assertSame($user->last_login_date->format('Y-m-d'), $result->last_login_date->format('Y-m-d'));
+		self::assertSame($user->is_deactivated, $result->is_deactivated);
+	}
 	
-	//todo deserialize_1_0 tests, succeess cases, and error cases for each field
+	public function testUser_Deserialize_1_0_IdField() : void
+	{
+		$user = $this->build_user();
+
+		//Arrange Leading Zero
+		$line = "01|20230131|0|jack|abcde|j@gmail.com|1234567890|20230315|0";
+		$format = "1.0";
+		//Act
+		$result = Eft_User::deserialize($line, $format);
+		//Assert
+		self::assertSame(1, $result->id);
+
+		//Arrange Large Int
+		$line = "999999999|20230131|0|jack|abcde|j@gmail.com|1234567890|20230315|0";
+		$format = "1.0";
+		//Act
+		$result = Eft_User::deserialize($line, $format);
+		//Assert
+		self::assertSame(999999999, $result->id);
+
+		//Arrange No Id Field - Defaults To Zero
+		$line = "|20230131|0|jack|abcde|j@gmail.com|1234567890|20230315|0";
+		$format = "1.0";
+		//Act
+		$result = Eft_User::deserialize($line, $format);
+		//Assert
+		self::assertSame(0, $result->id);
+	}
+	
+	//todo deserialize tests, succeess cases, and error cases for each field
 	
 	//todo move "1.0" magic string to constants
+	
+	//todo test if username or password hash contains pipe
+	
+	////////////////////////////////////////
+	
+	private function build_user() : Eft_User {
+		$user = new Eft_User();
+		$user->id = 1;
+		$user->created_date = DateTime::createFromFormat('Ymd', '20230131');
+		$user->is_admin = False;
+		$user->username = "jack";
+		$user->password_hashed = "abcde";
+		$user->email = "j@gmail.com";
+		$user->phone_number = "1234567890";
+		$user->last_login_date = DateTime::createFromFormat('Ymd', '20230315');
+		$user->is_deactivated = False;
+		return $user;
+	}
 }
 
 ?>
